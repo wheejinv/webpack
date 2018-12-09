38e891bc3baccd5bcac2ddb79fb0efbc350d06ea
whee <wheejinv@naver.com>
2018-12-09 17:01:36 +0900

    1. env 넣는 방법 2. #inline-source-map - 소스맵

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
-module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve( __dirname, 'dist' ),
-		filename: 'outputWebpack.bundle.js'
+// option 주는 방법
+// webpack -p --env d
+
+module.exports = ( env ) => {
+	console.warn( "env : ", env );
+	return {
+		entry: "./src/index.js",
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'outputWebpack.bundle.js'
+		},
+		devtool: env ? false : '#inline-source-map'
 	}
 };
